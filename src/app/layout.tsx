de05b309677b3b442e4307cf92e7e1cfa05eff2f
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2025-05-09 11:46:39 -0700

    feat: implement some components from shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
+import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = Geist({
@@ -31,14 +33,17 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
-            </Link>
+            <Button variant="ghost" asChild>
+              <Link href={homePath()}>
+                <LucideKanban />
+                <h1 className="text-lg font-semibold">TicketBounty</h1>
+              </Link>
+            </Button>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
-              Tickets
-            </Link>
+            <Button variant="default" asChild>
+              <Link href={ticketsPath()}>Tickets</Link>
+            </Button>
           </div>
         </nav>
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
