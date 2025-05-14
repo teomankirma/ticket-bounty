2fae5bce09d3297f66a03d24761871f84f4ee129
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2025-05-14 07:21:04 -0700

    feat: error boundary

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Heading } from "@/components/heading";
+import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
@@ -7,9 +9,12 @@ const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <Suspense fallback={<Spinner />}>
-        <TicketList />
-      </Suspense>
+
+      <ErrorBoundary fallback={<Placeholder label="Something went wrong!" />}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
